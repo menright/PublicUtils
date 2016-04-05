61c52175aac5971140b0196341c53482d37734ab
Madeline <me@aaichicago.com>
2016-04-05 11:58:15 -0700

    Added flags for product detail and output files
    -Refactored main to use argparser for better input control/validation
    -Added --show_items flag.  If true, all product names for the specified box will be output
    -Added --output_file flag.  If set, all outputs from this script will be written to the specified file name.  If not, output will be displayed on the command line.

--- getBBSpoilers.py
+++ getBBSpoilers.py
@@ -1,20 +1,49 @@
 #Birchbox spoilers util by ThePolymath
 #2016
-
 import requests
+import urllib
 import sys
+from bs4 import BeautifulSoup
+import argparse
+
 
 def main(args):
-    newStr = args[1] + "-" + args[2];
+    box_date = args.month + "-" + args.year;
     host = "https://www.birchbox.com/shop/birchbox-1/{d}/{d}-bb"
-    host = host.replace("{d}", newStr);
+    host = host.replace("{d}", box_date);
     for x in range(1,71):
-        response = requests.head(host + str(x))
+        box_url = host + str(x)
+        response = requests.head(box_url)
         if response.status_code != 404:
-            print host + str(x) + "\n"
+            print box_url
+            if args.show_items:
+              	html = urllib.urlopen(box_url).read()
+              	soup = BeautifulSoup(html, "html.parser")
+              	for product in soup.findAll("a", { "class" : "product_name track-event" }):
+          	        print product.contents[0].encode('utf-8').strip()
+              	print "\n"
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print "Usage: python getBBSpoilers.py month year \n example: python getBBSpoilers.py april 2016"
+    parser = argparse.ArgumentParser(description='Print boxes for a given month and year.')
+    parser.add_argument('--month', required=True, help='Month of box.')
+    parser.add_argument('--year', required=True, help='Year of box.')
+    parser.add_argument('--show_items', required=False, 
+                      default=False, help='If True, print the products contained in each box.')
+    parser.add_argument('--output_file', required=False, 
+                      default=False, help='If specified, write output to specified text file.  If flag is not used, data will be printed to console')
+    args = parser.parse_args()
+
+    if args.output_file:
+        print "Writing boxes to file " + args.output_file
+        orig_stdout = sys.stdout
+        f = file(args.output_file, 'w')
+        sys.stdout = f
+        main(args)
+        sys.stdout = orig_stdout
+        f.close()
     else:
-        main(sys.argv)
+        print "Writing boxes to cmdline"
+        main(args)
+
+    print "Box searching has completed"
